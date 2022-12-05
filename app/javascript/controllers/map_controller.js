71311332aa21580744bb1ea16441adf3849f8234
Grazia Cabras <graziacabras.gcabras@gmail.com>
2022-12-05 11:10:35 +0100

    Partial updates

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -22,13 +22,13 @@ export default class extends Controller {
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const customMarker = document.createElement('div')
-      customMarker.className = 'marker'
-      customMarker.style.backgroudImage = `url('${marker.image_url}')`
+      const dragonMarker = document.createElement('div')
+      dragonMarker.className = 'marker'
+      dragonMarker.style.backgroudImage = `url('${marker.image_url}')`
       console.log(marker.image_url)
-      customMarker.style.backgroundSize = 'contain'
-      customMarker.style.width = '25px'
-      customMarker.style.height = '25px'
+      dragonMarker.style.backgroundSize = 'contain'
+      dragonMarker.style.width = '25px'
+      dragonMarker.style.height = '25px'
 
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,45 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require 'open-uri'
 require_relative 'seed_location'
+require_relative 'seed_characters'
 
 puts "Seeding the DB..."
 
 puts "Seeding Locations..."
 seeding_location
 
-rhaenyra = User.create!(
-  name: 'Rhaenyra',
-  family_name: 'Targaryen',
-  email: 'rhaenyra@dragonstone.ws',
-  password: 'dracarys',
-  password_confirmation: 'dracarys'
-)
-
-daemon = User.create!(
-  name: 'Daemon',
-  family_name: 'Targaryen',
-  email: 'daemon@dragonstone.ws',
-  password: 'dracarys',
-  password_confirmation: 'dracarys'
-)
-
-syrax = Dragon.new(
-  name: 'Syrax'
-)
-
-caraxes = Dragon.new(
-  name: 'Caraxes'
-)
-
-syrax.user = rhaenyra
-syrax_file = URI.open('https://awoiaf.westeros.org/images/thumb/0/06/Syrax.jpeg/1200px-Syrax.jpeg')
-syrax.photos.attach(io: syrax_file, filename: 'syrax.png', content_type: 'image/png')
-syrax.location = Location.all.sample
-syrax.save!
-caraxes.user = daemon
-caraxes_file = URI.open('https://awoiaf.westeros.org/images/thumb/2/2f/TV_Caraxes.jpeg/800px-TV_Caraxes.jpeg')
-caraxes.photos.attach(io: caraxes_file, filename: 'caraxes.png', content_type: 'image/png')
-caraxes.location = Location.all.sample
-caraxes.save!
+puts "Seeding characters..."
