4507155c5c330036c49ca112cf974d6badae3bc5
Grazia Cabras <graziacabras.gcabras@gmail.com>
2022-12-06 15:31:34 +0100

    Seeded dragons and added cards to be fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_03_130038) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_142107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_03_130038) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "location_id"
+    t.text "description"
     t.index ["location_id"], name: "index_dragons_on_location_id"
     t.index ["user_id"], name: "index_dragons_on_user_id"
   end
